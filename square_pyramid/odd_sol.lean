58545fa2cbd043ee08cc66f797254db5fbfe8c7e
int-y1 <jason_yuen2007@hotmail.com>
2023-09-29 22:27:50 -0700

    add lemma 7

--- square_pyramid/odd_sol.lean
+++ square_pyramid/odd_sol.lean
@@ -2,12 +2,13 @@
 -- reference is doi:10.2307/2323911
 -- this is an in-progress attempt at formalizing the proof
 
+import Mathlib.Data.Int.ModEq
 import Mathlib.NumberTheory.PellMatiyasevic
 
 def u (n : ℕ) : ℤ := Pell.xz (by decide : 1 < 2) n
 def v (n : ℕ) : ℤ := Pell.yz (by decide : 1 < 2) n
 
-variable {m n : ℕ}
+variable {m n r : ℕ}
 
 theorem uv_pell_eq : u m ^ 2 - 3 * v m ^ 2 = 1 := by
   rw [u, v, pow_two, pow_two, ← mul_assoc, ← Pell.pell_eqz (by decide : 1 < 2) m]; rfl
@@ -44,6 +45,36 @@ theorem u_two_mul : u (2 * m) = 2 * u m ^ 2 - 1 ∧ u (2 * m) = 6 * v m ^ 2 + 1 
 theorem v_two_mul : v (2 * m) = 2 * u m * v m := by
   rw [two_mul, v_add]; ring
 
+/-- Lemma 7. I have no idea how to name this lemma. -/
+theorem lemma7 (h : n ≤ 2 * r * m) :
+    u (2 * r * m - n) ≡ (-1) ^ r * u n [ZMOD u m] ∧
+    u (2 * r * m + n) ≡ (-1) ^ r * u n [ZMOD u m] := by
+  have hv2m : v (2 * m) ≡ 0 [ZMOD u m] := by
+    rw [two_mul, v_add, ← left_distrib, Int.modEq_zero_iff_dvd]; apply dvd_mul_right
+  have hu : ∀ r, u ((2 * r + 1) * m) ≡ 0 [ZMOD u m] ∧ v (2 * r * m) ≡ 0 [ZMOD u m] := by
+    intro r
+    induction' r with r hr
+    · rw [Nat.mul_zero, zero_add, one_mul, zero_mul, Int.ModEq, Int.emod_self]; trivial
+    constructor
+    · rw [Nat.mul_succ, (by ring : (2 * r + 2 + 1) * m = (2 * r + 1) * m + 2 * m), u_add]
+      convert (hr.1.mul_right (u (2 * m))).add (hv2m.mul_left (3 * v ((2 * r + 1) * m)))
+      simp
+    · rw [Nat.mul_succ, right_distrib, v_add]
+      exact (hv2m.mul_left (u (2 * r * m))).add (hr.2.mul_left (u (2 * m)))
+  have : u (2 * r * m) ≡ (-1) ^ r [ZMOD u m] := by
+    have := u_two_mul (m := r * m)
+    rcases r.even_or_odd with ⟨r', hr'⟩ | ⟨r', hr'⟩
+    · rw [Even.neg_pow ⟨r', hr'⟩, one_pow, mul_assoc, this.2, hr', ← two_mul]
+      exact (((hu r').2.pow 2).mul_left 6).add_right 1
+    · rw [Odd.neg_pow ⟨r', hr'⟩, one_pow, mul_assoc, this.1, hr']
+      exact (((hu r').1.pow 2).mul_left 2).sub_right 1
+  constructor
+  · rw [u_sub h]
+    convert (this.mul_right (u n)).sub (((hu r).2.mul_left 3).mul_right (v n)) using 1
+    rw [mul_zero, zero_mul, sub_zero]
+  · rw [u_add]
+    convert (this.mul_right (u n)).add (((hu r).2.mul_left 3).mul_right (v n)) using 1
+    rw [mul_zero, zero_mul, add_zero]
 
 
 
